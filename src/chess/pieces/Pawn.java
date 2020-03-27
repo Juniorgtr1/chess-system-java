1f3b11929cff43a8bda4c46d19755b4eeb754271
Juniorgtr1 <juniorgtr1@gmail.com>
2020-03-27 16:00:17 -0300

    Special move - En Passant

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
 
-	public Pawn(Board board, Color color) {
+	private ChessMatch chassMatch;
+
+	public Pawn(Board board, Color color, ChessMatch chassMatch) {
 		super(board, color);
+		this.chassMatch = chassMatch;
 	}
 
 	@Override
@@ -38,8 +42,21 @@ public class Pawn extends ChessPiece {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 
-		}
-		else {
+			// #specialmove en passant white
+			if (position.getRow() == 3) {
+				Position left = new Position(position.getRow(), position.getColumn() - 1);
+				if (getBoard().positionExists(left) && isThereOponentPiece(left)
+						&& getBoard().piece(left) == chassMatch.getEnPassantVulnerable()) {
+					mat[left.getRow() - 1][left.getColumn()] = true;
+				}
+				Position right = new Position(position.getRow(), position.getColumn() + 1);
+				if (getBoard().positionExists(right) && isThereOponentPiece(right)
+						&& getBoard().piece(right) == chassMatch.getEnPassantVulnerable()) {
+					mat[right.getRow() - 1][right.getColumn()] = true;
+				}
+			}
+
+		} else {
 			p.setValues(position.getRow() + 1, position.getColumn());
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
@@ -58,11 +75,25 @@ public class Pawn extends ChessPiece {
 			if (getBoard().positionExists(p) && isThereOponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
+
+			// #specialmove en passant black
+			if (position.getRow() == 4) {
+				Position left = new Position(position.getRow(), position.getColumn() - 1);
+				if (getBoard().positionExists(left) && isThereOponentPiece(left)
+						&& getBoard().piece(left) == chassMatch.getEnPassantVulnerable()) {
+					mat[left.getRow() + 1][left.getColumn()] = true;
+				}
+				Position right = new Position(position.getRow(), position.getColumn() + 1);
+				if (getBoard().positionExists(right) && isThereOponentPiece(right)
+						&& getBoard().piece(right) == chassMatch.getEnPassantVulnerable()) {
+					mat[right.getRow() + 1][right.getColumn()] = true;
+				}
+			}
 		}
 
 		return mat;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "P";
